4eae5a9223521c1fa9bf1f5744c4028c74df2c18
WILLIAMS RUANO <wgruano@correo.url.edu.gt>
2018-10-21 19:59:14 -0600

    metodo para enviar correos

--- Chamuscopolis/Controllers/reservacionsController.cs
+++ Chamuscopolis/Controllers/reservacionsController.cs
@@ -12,6 +12,10 @@ using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using Chamuscopolis.Models;
 using Newtonsoft.Json.Linq;
+// using SendGrid's C# Library
+// https://github.com/sendgrid/sendgrid-csharp
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace Chamuscopolis.Controllers
 {
@@ -149,7 +153,22 @@ namespace Chamuscopolis.Controllers
                 }
             }
             //Aqui va el envio de un correo.
+            Execute().Wait();
             return CreatedAtRoute("DefaultApi", new { id = reservacion.idRESERVACION }, reservacion);
+        }
+
+        // send email method
+        static async Task Execute()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("test@example.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
         }
 
         // DELETE: api/reservacions/5
